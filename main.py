2433220a2dea1bf3250c1662bf5f4c01f02a8669
Arvind-Bansal <154428220+Arvind-Bansal@users.noreply.github.com>
2026-04-10 03:33:36 +0530

    Implement withdraw, view details, update details, and delete account methods
    - Add withdraw_money() method to withdraw funds from account
    - Add show_details() method to display account information
    - Add update_details() method to update account details
    - Add delete_account() method to delete an account with confirmation
    - Add debug output to deposit_money() for troubleshooting

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ class Bank:
         except Exception as err:
             print(f"Error occurred: {err}")
 
+ 
+ 
+ 
     @classmethod
     def update(cls):   #THIS FUNCTION IS USED TO UPDATE THE DATA IN THE JSON FILE
         try:
@@ -28,6 +31,9 @@ class Bank:
                 fs.write(json.dumps(cls.data)) #dumping the data into the json file
         except Exception as err:
             print(f"Error occurred: {err}")
+
+
+
 
     @staticmethod
     def __accountgenerate(): #THIS FUNCTION IS USED TO GENERATE A RANDOM ACCOUNT NUMBER
@@ -38,6 +44,11 @@ class Bank:
         random.shuffle(id) #shuffling the account number to make it more random 
 
         return "".join(id) #joining the list of characters to form a string and returning it
+
+
+
+
+
     def create_account(self):
         info= {
             "name": input("enter your name: "),
@@ -59,6 +70,10 @@ class Bank:
             Bank.data.append(info) #appending the data to the data list
 
             Bank.update()
+
+
+
+
 
     def deposit_money(self):
         account_number=input("enter your account number: ")
@@ -84,6 +99,8 @@ class Bank:
                 print("amount deposited successfully")
 
 
+
+
     def withdraw_money(self):
         account_number=input("enter your account number: ")
         pin=int(input("enter your pin: "))
@@ -101,6 +118,86 @@ class Bank:
                 userdata[0]['balance']-=amount #subtracting the amount from the balance
                 Bank.update() #updating the json file with the new balance
                 print("amount withdrawn successfully")
+
+
+
+
+
+    def show_details(self):
+        account_number=input("enter your account number: ")
+        pin=int(input("enter your pin: "))
+
+        userdata=[i for i in Bank.data if str(i["account_number"]) == account_number and i["pin"] == pin] #checking if the account number and pin matches with the data in the json file
+        
+        if not userdata:
+            print("invalid account number or pin")
+        else:
+            for i in userdata[0]:
+                print(f"{i}: {userdata[0][i]}") #printing the details of the account
+
+
+
+    def update_details(self):
+        account_number=input("enter your account number: ")
+        pin=int(input("enter your pin: "))
+
+        userdata=[i for i in Bank.data if str(i["account_number"]) == account_number and i["pin"] == pin] #checking if the account number and pin matches with the data in the json file
+        
+        if not userdata:
+            print("invalid account number or pin")
+        else:
+           print("you can't change the age,account number and balance of the account")
+           
+           print("fill the details for the fields you want to update and leave the rest blank")
+
+           newdata={
+            "name": input("enter your name: ") or userdata[0]['name'], #if the user leaves the input blank, it will keep the old value
+            "email": input("enter your email: ") or userdata[0]['email'], #if the user leaves the input blank, it will keep the old value
+            "pin": int(input("enter your pin: ") or userdata[0]['pin']), #if the user leaves the input blank, it will keep the old value
+            "age": userdata[0]['age'], #keeping the age same as the old value
+            "account_number": userdata[0]['account_number'], #keeping the account number same as
+            "balance": userdata[0]['balance'] #keeping the balance same as the old value
+           }
+
+
+           if type(newdata['pin'])==str:
+                newdata['pin']=int(newdata['pin']) #converting the pin to integer if the user enters a string
+
+
+           for i in newdata:
+            if newdata[i] == userdata[0][i]:
+                continue
+            else:
+                userdata[0][i]=newdata[i] #updating the details of the account
+            
+            
+            Bank.update() #updating the json file with the new details
+            print(f"{i} updated successfully")
+
+
+
+
+
+
+    def delete_account(self):
+        account_number=input("enter your account number: ")
+        pin=int(input("enter your pin: "))
+
+        userdata=[i for i in Bank.data if str(i["account_number"]) == account_number and i["pin"] == pin] #checking if the account number and pin matches with the data in the json file
+        
+        if not userdata:
+            print("invalid account number or pin")
+        else:
+            check=input("are you sure you want to delete your account? (y/n): ")
+            if check=='n' or check=='N':
+                print("account deletion cancelled")
+            else:
+                Bank.data.remove(userdata[0]) #removing the account from the data list
+                Bank.update() #updating the json file after deleting the account
+                print("account deleted successfully")
+
+
+
 
 
         
@@ -122,3 +219,12 @@ elif check==2:
 
 elif check==3:
     user.withdraw_money()
+
+elif check==4:
+    user.show_details()
+
+elif check==5:
+    user.update_details()
+
+elif check==6:
+    user.delete_account()
